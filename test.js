47026f40df58c53cd761b42d6ba15fc4bb3e3135
r1r2 <33709032+r1r2@users.noreply.github.com>
2020-04-28 09:49:12 +0545

    Add files via upload

--- test.js
+++ test.js
@@ -2,12 +2,17 @@
 "use strict";
 
 function percentage() {
-    var total = Number(document.getElementById('total').value);
     var dead = Number(document.getElementById('dead').value);
     var recover = Number(document.getElementById('recover').value);
-
-    document.getElementById('resultdead').innerHTML = Math.round((dead/total)*100);
-    document.getElementById('resultrecover').innerHTML = Math.round((recover/total)*100);
+    var serious = Number(document.getElementById('serious').value);
+    var normal = Number(document.getElementById('normal').value);
+    
+    var total = dead + recover;
+    var active = serious + normal;
+    document.getElementById('resultdead').innerHTML = Math.round((dead/total)*100) + "  %";
+    document.getElementById('resultrecover').innerHTML = Math.round((recover/total)*100) + "  %";
+    document.getElementById('resultserious').innerHTML = Math.round((serious/active)*100) + "  %";
+    document.getElementById('resultnormal').innerHTML = Math.round((normal/active)*100) + "  %";
     
     return false;
 }
